eff95d4fcdb1bae3dba9d42bf202891cd921ca2b
kikuchi <kikuchi@16bit.co.jp>
2020-03-17 20:05:38 +0900

    fix

--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
-// const PORT = process.env.PORT || 8080
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const history = require('connect-history-api-fallback');
 
 const app = express();
